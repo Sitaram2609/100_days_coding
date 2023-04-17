1e257a7e3f8b78982526680dd1e3ea95fb625e04
SitaRam <100852293+Sitaram2609@users.noreply.github.com>
2023-04-17 18:41:31 +0530

    Create switch.day5.js
    switch statement

---
+++ switch.day5.js
@@ -0,0 +1,15 @@
+const day=parseInt(prompt("ENTER"))
+
+switch(day){
+
+    case 1:
+      console.log("MONDAY")
+      break;
+    case 2:
+       console.log("TUESDAY")
+       break;
+    case 3 :
+        console.log("WEDNESDAY")
+    default:
+        console.log("NOT FOUND")          
+}
