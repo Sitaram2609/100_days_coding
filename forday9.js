3fd76948c6fc4f77e339dff7c77ef56ad5051ec9
SitaRam <100852293+Sitaram2609@users.noreply.github.com>
2023-04-27 21:43:59 +0530

    Create forday9.js

---
+++ forday9.js
@@ -0,0 +1,7 @@
+const people = ["Scooby", "Velma", "Daphne", "Shaggy", "Fred"]; //DONT TOUCH THIS LINE!
+
+// WRITE YOUR LOOP BELOW THIS LINE
+
+for( let i=people.length-1;i=>0;i--){
+  console.log(people[i].toUpperCase()) ;
+}
